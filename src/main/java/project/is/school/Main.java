4e8994d65241708062060b7f3a2839db3521b7f5
rcnpri <raccoonperi08@gmail.com>
2024-01-08 02:10:54 +0500

    Initial commit

---
+++ src/main/java/project/is/school/Main.java
@@ -0,0 +1,29 @@
+package project.is.school;
+
+import project.is.school.dto.Schools;
+
+import java.util.List;
+
+public class Main {
+    public static void main (String[] args) throws Exception{
+        Parser parse = new Parser();
+        List<Schools> school = parse.parseCsv("Школы.csv");
+        //System.out.println(parse.parseCsv("Школы.csv"));
+
+        database db = new database();
+        db.openConnection("is");
+        db.saveSchools(school);
+
+        // Вывод среднего значения расходов
+        db.AverageExpenditureByCountries();
+
+        // Вывод учебного заведения с заданными критериями
+        db.SchoolsByCriteria();
+
+        // Расчет    среднего количества студентов по странам
+        db.calculateAverageStudentsByCountry();
+
+        db.close();
+
+    }
+}
